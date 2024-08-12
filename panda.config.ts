1b433cdc79746fc69167045f2ca5eeda7a469344
lee-sihun <lee.sihun040223@gmail.com>
2024-08-12 15:14:29 +0900

    feat: 컬러 팔레트 추가

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,32 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          // 사용 예시 -> background: 'primary.01'
+          primary: {
+            '01': { value: '#50596B' },
+            '02': { value: '#EBE7DE' },
+            '03': { value: '#FAD56B' } // Yellow 01
+          },
+          secondary: {
+            '01': { value: '#CE7866' },
+            '02': { value: '#AD8E74' },
+            '03': { value: '#F3EEE6' },
+            '04': { value: '#DED7CE' }, // 호버 전
+            '05': { value: '#C8B59C' } // 호버 후
+          },
+          gray: {
+            '01': { value: '#161616' },
+            '02': { value: '#707070' },
+            '03': { value: '#9A9A9A' },
+            '04': { value: '#C4C4C4' },
+            '05': { value: '#E7E7E7' },
+            '06': { value: '#F4F4F4' }
+          }
+        }
+    }},
   },
 
   // The output directory for your css system
