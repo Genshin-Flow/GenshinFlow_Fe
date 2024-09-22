9761dd37711c889ec434f1fb6d6ce18733c0bcec
lee-sihun <lee.sihun040223@gmail.com>
2024-09-22 21:58:29 +0900

    feat: 메인 레이아웃

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import MatchingPostItem from "@/features/matching/components/MatchingPostItem";
+import { styled } from "../../styled-system/jsx";
+import Tab from "@/features/matching/components/Tab/";
+import Sidebar from "@/features/matching/components/Sidebar";
 
 export default function Home() {
 	return (
-		<div>
-			<MatchingPostItem />
-		</div>
+		<Container>
+			<Tab />
+			<Sidebar />
+		</Container>
 	);
 }
+
+const Container = styled("div", {
+	base: {
+		display: "flex",
+		maxWidth: "1639px",
+		minWidth: "1318px",
+		mx: "auto",
+		justifyContent: "space-between",
+		gap: "20px",
+		padding: "40px 40px",
+	},
+});
