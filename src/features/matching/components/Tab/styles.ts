f07199e0b0d8051741dc2aabb5d35e48fc1f8428
lee-sihun <lee.sihun040223@gmail.com>
2024-09-22 17:26:14 +0900

    feat: 탭 추가

--- src/features/matching/components/Tab/styles.ts
+++ src/features/matching/components/Tab/styles.ts
@@ -1,13 +1,36 @@
 import { styled } from "../../../../../styled-system/jsx";
 
+const flexCenter = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100%",
+};
+
+const fontStyle = {
+  color: "gray.01",
+  fontSize: "14px",
+  fontWeight: "bold",
+};
+
+const flexItem = {
+  ...flexCenter,
+  ...fontStyle,
+  position: "relative",
+  cursor: "pointer",
+  flex: '1 1 auto',
+};
+
 export const Container = styled("div", {
   base: {
     display: "flex",
     flexDirection: "column",
     maxWidth: "1129px",
     minWidth: "888px",
-    maxHeight: "797px",
+    height: "797px",
     minHeight: "660px",
+    backgroundColor: "primary.02",
+    overflow: "hidden",
   },
 });
 
@@ -21,6 +44,7 @@ export const TabContainer = styled("div", {
     backgroundColor: "primary.01",
     padding: "4px 5px 5px 5px", 
     position: "relative", 
+    flexShrink: 0,
     "&::before": {
       content: '""',
       position: "absolute",
@@ -78,4 +102,224 @@ export const TabItem = styled("div", {
       },
     },
   },
+});
+
+export const MatchingMenu = styled("div", {
+  base: {
+    display: "flex",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 30px",
+    marginTop: "30px",
+    marginBottom: "30px",
+    flexShrink: 0,
+    "& div": {
+      display: "flex",
+      alignItems: "center",
+      gap: "20px",
+    }
+  },
+});
+
+export const WriteButton = styled("button", {
+  base: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "113px",
+    height: "40px",
+    backgroundColor: "primary.01",
+    color: "secondary.03",
+    fontSize: "14px",
+    fontWeight: "medium",
+    letterSpacing: "0",
+    borderRadius: "4px",
+    cursor: "pointer",
+    border: "1px solid #696969",
+    backgroundImage: "url('/svgs/write.svg')",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "24px 24px",
+    backgroundPosition: "10px center",
+    paddingLeft: "35px",
+    boxSizing: "border-box",
+  },
+});
+
+export const MatchingHeader = styled("div", {
+  base: {
+    display: "flex",
+    alignItems: "center",
+    maxWidth: "1124px",
+    minWidth: "883px",
+    width: "100%",
+    height: "32px",
+    marginLeft: "5px",
+    backgroundColor: "gray.06",
+    flexShrink: 0,
+  },
+});
+
+export const UserName = styled("div", {
+  base: {
+    ...flexItem,
+    flex: '1 1 200px',
+  },
+  variants: {
+    select: {
+      true: {
+        color: "secondary.01",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "53px", // 37 + 16
+          height: "2px", 
+          backgroundColor: "secondary.01", 
+        },
+      }
+    }
+  }
+});
+
+export const QuestType = styled(UserName, {
+  base: {
+  },
+  variants: {
+    select: {
+      true: {
+        color: "secondary.01",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "80px", // 64 + 16
+          height: "2px", 
+          backgroundColor: "secondary.01", 
+        },
+      }
+    }
+  }
+});
+
+export const WorldLevel = styled('div', {
+  base: {
+    ...flexItem,
+    flex: '1 1 100px',
+  },
+  variants: {
+    select: {
+      true: {
+        color: "secondary.01",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "68px", // 52 + 16
+          height: "2px", 
+          backgroundColor: "secondary.01", 
+        },
+      }
+    }
+  }
+});
+
+export const Message = styled('div', {
+  base: {
+    ...flexItem,
+    flex: '1 1 386px',
+  },
+  variants: {
+    select: {
+      true: {
+        color: "secondary.01",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "53px", // 37 + 16
+          height: "2px", 
+          backgroundColor: "secondary.01", 
+        },
+      }
+    }
+  }
+});
+
+export const TimeAgo = styled('div', {
+  base: {
+    ...flexItem,
+    flex: '1 1 140px',
+  },
+  variants: {
+    select: {
+      true: {
+        color: "secondary.01",
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "65px", // 49 + 16
+          height: "2px", 
+          backgroundColor: "secondary.01", 
+        },
+      }
+    }
+  }
+});
+
+export const MoreOptions = styled('div', {
+  base: {
+    ...flexItem,
+    flex: '1 1 90px',
+  }
+});
+
+export const Gap = styled('div', {
+  base: {
+    width: "8px",
+    height: "100%",
+  }
+});
+
+export const PostList = styled('div', {
+  base: {
+    marginTop: "1px",
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+    overflowY: "auto",
+    mx: "auto",
+    flexGrow: 1,
+    paddingLeft: "5px",
+    "&::-webkit-scrollbar": {
+      width: "8px",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      width: "6px",
+      backgroundColor: "#F4F4F4",
+      borderRadius: "2px",
+      border: "1px solid transparent",
+      backgroundClip: "padding-box",
+      backgroundImage:  "url('/svgs/scroll.svg')",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "4px calc(100% - 2px)",
+      backgroundPosition: "center center",
+    },
+    "&::-webkit-scrollbar-track": {
+      backgroundColor: "gray.01",
+      borderRadius: "2px",
+    },
+  }
 });
