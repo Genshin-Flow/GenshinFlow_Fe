8e4014401e9870995d0d6ab050f3be54dd06fb24
fefdfea <cankyu6@gmail.com>
2024-10-06 15:22:30 +0900

    feature/PC 마이페이지 제작

--- src/features/mypage/components/userInfo/UserInfo.tsx
+++ src/features/mypage/components/userInfo/UserInfo.tsx
@@ -1,11 +1,27 @@
-import MypageText from "@/features/mypage/components/mypageText/MypageText";
-import UserInfoBox from "@/features/mypage/components/userInfo/UserInfoBox";
+import { styled } from "@/../styled-system/jsx";
+import UserInfoItem from "@/features/mypage/components/userInfo/UserInfoItem";
 
 export default function UserInfo() {
 	return (
-		<>
-			<MypageText mt="mt40">유저 정보</MypageText>
-			<UserInfoBox />
-		</>
+		<UserInfoContainer>
+			<UserInfoItem name={"이메일"} children={"cankyu6@gmail.com"} />
+			<UserInfoItem
+				name={"비밀번호"}
+				children={"1004ff1!"}
+				password={true}
+				buttonText="변경"
+			/>
+			<UserInfoItem name={"UID"} children={"12345678"} />
+		</UserInfoContainer>
 	);
 }
+
+const UserInfoContainer = styled("article", {
+	base: {
+		width: "100%",
+		padding: "12px",
+		backgroundColor: "gray.06",
+		borderRadius: "8px",
+		border: "1px solid {colors.gray.04}",
+	},
+});
