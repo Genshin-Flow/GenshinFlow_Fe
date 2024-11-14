a80f1c6434b678645af6685efb6c5e59bf7ef94f
fefdfea <cankyu6@gmail.com>
2024-11-15 00:54:21 +0900

    fix/모바일 로그인 url 재수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -48,7 +48,7 @@ export default function Header({ isMobile = false }: HeaderProps) {
 					</Link>
 					<Btns>
 						<Button style="menu" />
-						<Link href="/MobileLogin">
+						<Link href="/Login">
 							<Button style="login" />
 						</Link>
 					</Btns>
