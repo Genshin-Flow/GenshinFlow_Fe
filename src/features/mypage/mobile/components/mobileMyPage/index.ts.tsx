68170a446afdf8c094326947b8bc039534723597
fefdfea <cankyu6@gmail.com>
2025-01-20 13:22:55 +0900

    fix/모바일 로그아웃 적용

--- src/features/mypage/mobile/components/mobileMyPage/index.ts.tsx
+++ src/features/mypage/mobile/components/mobileMyPage/index.ts.tsx
@@ -2,18 +2,14 @@ import UserProfile from "@/features/mypage/components/userInfoGroup/userProfile"
 import AccountInfo from "@/features/mypage/mobile/components/accoundGroup/accountInfo";
 import UserInfo from "@/features/mypage/components/userInfoGroup/userInfo";
 import History from "@/features/mypage/components/historyGroup/historyInfo";
-import Link from "next/link";
-import { MyPageContainer, ExitButton } from "./style";
+import { MyPageContainer } from "./style";
+import ExitButton from "@/features/mypage/mobile/components/mobileExitButton";
 export default function MobileMypage() {
 	// 헤더 제작되면 추가
 
 	return (
 		<MyPageContainer className="MyPage">
-			<ExitButton>
-				<Link href={"/"}>
-					<img src="/svgs/exit.svg" alt="메인페이지로 이동" />
-				</Link>
-			</ExitButton>
+			<ExitButton />
 			<UserProfile />
 			<AccountInfo />
 			<UserInfo />
